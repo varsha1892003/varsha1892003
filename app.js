7b8e44343cc5623e5d74dbeb356bb96b8e3049f7
varsha1892003 <151153156+varsha1892003@users.noreply.github.com>
2023-11-20 11:24:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const express = require('express')
+var app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/", (req, res) => {
+    res.send("i am run at 5000")
+})
+
+app.listen(5000, '0.0.0.0')
