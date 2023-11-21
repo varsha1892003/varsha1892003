e90eaa8a61843d4554e1de32087484ceebb74a43
varsha1892003 <151153156+varsha1892003@users.noreply.github.com>
2023-11-21 13:15:48 +0530

    Delete hrms-api directory

--- hrms-api/src/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const mongoose = require('./services/mongoose')
-const app = require('./services/express')
-// start app and connect to database
-app.start()
-mongoose.connect()
-
-module.exports = app
